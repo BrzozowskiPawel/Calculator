d15f426cc671140fa63993fed96783e7837d8708
Paweł Brzozowski <mac@192.168.1.6>
2021-12-18 15:33:21 +0100

    added CalculatorManager

---
+++ Calculator/Calculator/CalculatorManager.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorManager.swift
+//  Calculator
+//
+//  Created by Paweł Brzozowski on 18/12/2021.
+//
+
+import Foundation
+
+class CalculatorManager {
+    func calculate(_ firstNumber: Double, secondNumber: Double, operation: Int) -> Double{
+        var total = 0.0
+        
+        switch operation {
+        case 0:
+            total =  firstNumber + secondNumber
+        case 1:
+            total =  firstNumber - secondNumber
+        case 2:
+            total =  firstNumber * secondNumber
+        case 3:
+            total =  firstNumber / secondNumber
+        default:
+            print("ERROR OPERATION INDEX")
+        }
+        
+        // Defult operation gives few decimal palces, we would like only 3 digits
+        return Double(floor(1000*total)/1000)
+    }
+}
