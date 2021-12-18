4318614d5713470fcf7d8adb3f4cdd3fda01f900
Paweł Brzozowski <mac@192.168.1.6>
2021-12-18 15:16:15 +0100

    added buttons (numerical) functionality

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberButtonClicked(_ sender: UIButton) {
+        var number = sender.titleLabel!.text!
+        resultLabel.text! += number
+        
+    }
+    
 }
 
